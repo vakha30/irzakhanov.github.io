174eb48a8df70e3f5c703630e6b0c53dcc7f92b4
mansur30 <47324041+mansur30@users.noreply.github.com>
2019-04-26 23:44:33 +0400

    monitoring

--- monitoring/js/script.js
+++ monitoring/js/script.js
@@ -1,14 +1,23 @@
-// $('.posts-slide').slick({
-// 	infinite: true,
-// 	slidesToShow: 3,
-// 	arrows: true,
-// 	dots: false,
-// 	centerMode: true,
-// 	responsive: [
-//     {
-//     	breakpoint: 800,
-//      	settings: {
-//         	slidesToShow: 1,
-//        	}
-//     }]
-// });
+$('#btn-skoro').on('click',function() {
+    $('#skoro').css('display','block');
+    $('#uje').css('display','none');
+    $('#btn-skoro').css('border-color', '#c5c204');
+    $('#btn-skoro').css('color', '#c5c204');
+
+    $('#btn-uje').css('border-color', '#333333');
+    $('#btn-uje').css('color', '#333333');
+})
+
+$('#btn-uje').on('click',function() {
+    $('#skoro').css('display','none');
+    $('#uje').css('display','block');
+    $('#btn-uje').css('border-color', '#c5c204');
+    $('#btn-uje').css('color', '#c5c204');
+
+    $('#btn-skoro').css('border-color', '#333333');
+    $('#btn-skoro').css('color', '#333333');
+})
+
+
+
+
