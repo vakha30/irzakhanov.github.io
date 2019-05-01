53ae8ffadfac74b71a0bd26ad15d80afa93d7f2c
mansur30 <47324041+mansur30@users.noreply.github.com>
2019-05-01 23:30:05 +0400

    buhone

---
+++ buhone/js/script.js
@@ -0,0 +1,20 @@
+$('.banner-slide').slick({
+	infinite: true,
+	slidesToShow: 1,
+	arrows: true,
+	dots: true
+});
+
+$('.clients-slide').slick({
+	infinite: true,
+	slidesToShow: 1,
+	arrows: true,
+	dots: true
+});
+
+$('.reviews-slide').slick({
+	infinite: true,
+	slidesToShow: 1,
+	arrows: true,
+	dots: true
+});
