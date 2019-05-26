f08ea1e3981b0d04d0feb978010f3ee03973e450
mansur30 <47324041+mansur30@users.noreply.github.com>
2019-05-26 22:57:04 +0400

    travel

---
+++ travel/js/script.js
@@ -0,0 +1,14 @@
+// $('.posts-slide').slick({
+// 	infinite: true,
+// 	slidesToShow: 3,
+// 	arrows: true,
+// 	dots: false,
+// 	centerMode: true,
+// 	responsive: [
+//     {
+//     	breakpoint: 800,
+//      	settings: {
+//         	slidesToShow: 1,
+//        	}
+//     }]
+// });
